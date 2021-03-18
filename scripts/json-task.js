58b47c9ad1e2c331c94c3427be80543d217ac6c0
Ivan Egorov <www.Len244@gmail.com>
2021-03-18 11:15:46 +0300

    Added input and currency switch functionality

--- scripts/json-task.js
+++ scripts/json-task.js
@@ -1,31 +1,32 @@
 const base_url = "https://www.cbr-xml-daily.ru/latest.js"
 
-function populateCurrentValues(jsonData){
+function populateCurrentValues(jsonData) {
     const toFill = document.getElementById("currency-curent-values")
 
-    for (item in jsonData["rates"]){
+    for (item in jsonData["rates"]) {
         const toAppendRow = document.createElement("tr")
 
         const toAppendCurrencyName = document.createElement("td")
-        toAppendCurrencyName.textContent = toAppendCurrencyName.id = item
+        toAppendCurrencyName.textContent = item
 
         const toAppendCurrencyValue = document.createElement("td")
         toAppendCurrencyValue.textContent = jsonData["rates"][item]
+        toAppendCurrencyValue.id = item
 
         toAppendRow.appendChild(toAppendCurrencyName)
         toAppendRow.appendChild(toAppendCurrencyValue)
 
         toFill.appendChild(toAppendRow)
-    }   
+    }
 }
 
-function populateCurrencySelector(jsonData){
+function populateCurrencySelector(jsonData) {
     const toFill = document.getElementById("currency-selector")
 
-    for (item in jsonData["rates"]){
+    for (item in jsonData["rates"]) {
         const toAppendOption = document.createElement("option")
 
-        toAppendOption.textContent = toAppendOption.id = item
+        toAppendOption.textContent = item
         toAppendOption.value = item
 
         toFill.appendChild(toAppendOption)
@@ -34,22 +35,59 @@ function populateCurrencySelector(jsonData){
     toFill.for
 }
 
-fetch(base_url).then(response => response.json()).then(json =>
-    {
-        populateCurrencySelector(json)
-        populateCurrentValues(json)
-    })
+fetch(base_url).then(response => response.json()).then(json => {
+    populateCurrencySelector(json)
+    populateCurrentValues(json)
+})
 
-var currency_input = document.getElementById("current-currency-input")
+const currency_input = document.getElementById("current-currency-input")
+
+const targetCurrencySelector = document.getElementById("currency-selector")
+
+const swapperButton = document.getElementById("swap-button")
 
 
-// TODO Add event listener to currency_input to auto calculation on prior currency
-// TODO Event listener for currency change
 // TODO Swap button. Decide to implement or delete
 
-currency_input.addEventListener("input", (event) =>{
-    console.log(event.target.value)
+function calculateInput(event) {
+    console.log(value)
 
+    const currencies = document.getElementsByClassName("currency_to_swap")
 
+    const value = document.getElementById("current-currency-input").value
+    const output = document.getElementById("current-currency-output")
 
-})
+    const inputCurrency = currencies[0].textContent
+    const outputCurrency = currencies[1].firstElementChild.value
+
+    const currencyRate = +document.getElementById(outputCurrency).textContent
+
+    console.log(currencyRate);
+
+    output.value = value * currencyRate
+}
+
+function changeTargetCurrency(event) {
+    const currencies = document.getElementsByClassName("currency_to_swap")
+
+    const value = document.getElementById("current-currency-input").value
+    const output = document.getElementById("current-currency-output")
+
+    const inputCurrency = currencies[0].textContent
+    const outputCurrency = currencies[1].firstElementChild.value
+
+    const currencyRate = +document.getElementById(outputCurrency).textContent
+
+    console.log(currencyRate);
+
+    output.value = value * currencyRate
+}
+
+function swapInputCurrency(event) {
+
+}
+
+currency_input.addEventListener("input", calculateInput)
+targetCurrencySelector.addEventListener("change", changeTargetCurrency)
+
+swapperButton.addEventListener("click", swapInputCurrency)
