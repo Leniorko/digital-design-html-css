a6265f9b71e78e08d4eee5c13d486e2f917008e3
Ivan Egorov <www.Len244@gmail.com>
2021-03-21 14:10:17 +0300

    Formated jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import CurrencyPrices from './components/CurrencyPrices';
-import CurrencyConverter from './components/CurrencyConverter'  
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import CurrencyPrices from "./components/CurrencyPrices";
+import CurrencyConverter from "./components/CurrencyConverter";
 
 function App() {
+  const [currencyRateJson, setCurrencyRate] = useState();
 
-  const [currencyRateJson, setCurrencyRate] = useState()
+  useEffect(
+    () =>
+      fetch("https://www.cbr-xml-daily.ru/latest.js")
+        .then((response) => response.json())
+        .then((result) => {
+          setCurrencyRate(result);
+        }),
+    []
+  );
 
-  useEffect(() => fetch("https://www.cbr-xml-daily.ru/latest.js").then(response => response.json()).then(result => {
-    setCurrencyRate(result)
-  }), [] )
-
-  function updateRates(){
-    fetch("https://www.cbr-xml-daily.ru/latest.js").then(response => response.json()).then(result => {setCurrencyRate(result)})
+  function updateRates() {
+    fetch("https://www.cbr-xml-daily.ru/latest.js")
+      .then((response) => response.json())
+      .then((result) => {
+        setCurrencyRate(result);
+      });
   }
-
 
   return currencyRateJson ? (
     <>
-      <CurrencyConverter currencyData = {currencyRateJson} updateRates={updateRates}/>
-      <CurrencyPrices currencyData = {currencyRateJson}/>
+      <CurrencyConverter
+        currencyData={currencyRateJson}
+        updateRates={updateRates}
+      />
+      <CurrencyPrices currencyData={currencyRateJson} />
     </>
-  ) : <></>
+  ) : (
+    <></>
+  );
 }
 
 export default App;
